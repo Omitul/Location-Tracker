37ab4d14df86679df04694aca3703a76d1132e1e
omitulclera <omitul1010@gmail.com>
2024-01-03 21:23:26 +0600

    Set the map link and change some color, although some color is not working,Try later

--- app/src/main/java/com/example/ltracker/MainActivity.java
+++ app/src/main/java/com/example/ltracker/MainActivity.java
@@ -14,10 +14,16 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
     EditText etBusId;
-    TextView tvLatitude, tvLongitude;
+    TextView tvLatitude, tvLongitude, tvmap;
     DatabaseReference reference;
 
     @Override
@@ -28,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         etBusId = findViewById(R.id.etbusid);
         tvLatitude = findViewById(R.id.tvLatitude);
         tvLongitude = findViewById(R.id.tvLongitude);
+        tvmap = findViewById(R.id.tvmap);
 
         findViewById(R.id.readdataBtn).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,19 +65,42 @@ public class MainActivity extends AppCompatActivity {
                         // Retrieve latitude and longitude directly
                         String latitude = busData.child("Latitude").getValue(String.class);
                         String longitude = busData.child("Longitude").getValue(String.class);
+                        String mapLink = "http://maps.google.com/maps?q=" + latitude + "," + longitude;
                         double Longitude = 0.0;
-                        String modifiedString = longitude.substring(1, longitude.length() - 1);
+                        String modifiedString = longitude.substring(0, longitude.length() - 1);
                         Longitude = Double.valueOf((modifiedString));
                         double Latitude = 0.0;
-                        modifiedString = latitude.substring(1, latitude.length() - 1);
+                        modifiedString = latitude.substring(0, latitude.length() - 1);
                         Latitude = Double.valueOf((modifiedString));
 
 
                         // setting the values in the field in string format, because only strings are allowed to set
                         tvLatitude.setText(String.valueOf(Latitude));
                         tvLongitude.setText(String.valueOf(Longitude));
+                        tvmap.setText(mapLink);
+                        Intent intent = new Intent(Intent.ACTION_VIEW);
+                        intent.setData(Uri.parse(mapLink));
 
-                        Toast.makeText(MainActivity.this, "Location Found!", Toast.LENGTH_LONG).show();
+                        tvmap.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                // Create an Intent with the ACTION_VIEW action
+                                Intent intent = new Intent(Intent.ACTION_VIEW);
+
+                                // Set the data of the Intent to the mapLink
+                                intent.setData(Uri.parse(mapLink));
+
+                                // Check if there is any application available to handle the Intent
+                                if (intent.resolveActivity(getPackageManager()) != null) {
+                                    // Start the activity if a suitable app is found
+                                    startActivity(intent);
+                                } else {
+                                    // Handle the case where Google Maps is not installed
+                                    // You can show a toast or a dialog prompting the user to install Google Maps
+                                    Toast.makeText(MainActivity.this, "Google Maps is not installed", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
                     } else {
                         Log.d("ReadData", "Data doesn't exist");
                         Toast.makeText(MainActivity.this, "Location didn't found!", Toast.LENGTH_LONG).show();
